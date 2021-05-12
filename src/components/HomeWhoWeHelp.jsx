04724ac3e65262449db54566a971811ca0e57db9
stanleykubrik1 <62182076+stanleykubrik1@users.noreply.github.com>
2021-05-12 23:49:47 +0200

    WhoWeHelp component start pagination

--- src/components/HomeWhoWeHelp.jsx
+++ src/components/HomeWhoWeHelp.jsx
@@ -1,18 +1,46 @@
 import React, {useState} from 'react';
 import DecorationTitle from './DecorationTitle';
-import ReactPaginate from 'react-paginate';
 import organizationsArr from './whoWeHelpDataBase.json';
 
 function HomeWhoWeHelp() {
-    const [organizations, setOrganizations] = useState(organizationsArr);
+    const [organizations, setOrganizations] = useState(organizationsArr.slice(0,3));
+    const [description, setDescription] = useState([]);
     console.log(organizations)
+
+    const changeContent = (start,end) => {
+        setOrganizations(organizationsArr.slice(start,end))
+    }
+
     return (
         <div className="who-we-help-container">
             <DecorationTitle title={"Komu pomagamy?"}/>
             <div className="btn-div-container">
-                <div className="btn">Fundacjom</div>
-                <div className="btn">Organizacją pozarządowym</div>
-                <div className="btn">Lokalnym zbiórką</div>
+                <div onClick={()=>changeContent(0,3)}  className="btn">Fundacjom</div>
+                <div onClick={()=>changeContent(3,6)} className="btn">Organizacją pozarządowym</div>
+                <div onClick={()=>changeContent(6,9)}  className="btn">Lokalnym zbiórką</div>
+            </div>
+            <div className="organizations-container">
+                <p className="main-description">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+                <div className="table-container">
+                    {organizations.map((el,i) => {
+                        console.log(el)
+                        return <>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th key={i}>{el.name}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td >{el.mission}</td>
+                                        <td >{el.description}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </>
+                    })}
+                </div>
             </div>
         </div>
     )
